f803cdf64446196622791600538fc09c15f688cb
frimpong1781 <frimpong1781@yahoo.com>
2020-11-20 23:59:51 +0000

    Create contacts reducer

---
+++ src/reducers/contactsReducer.js
@@ -0,0 +1,47 @@
+const initialState = {
+    contacts: [
+        {
+          first_name: "James",
+          last_name: "Frimpong",
+          middle_name: "Kwame",
+          phone_number: '0244626767',
+          address: "Box Mc 1582, Takoradi",
+          email: "jfrimpong@yahoo.com",
+          type: "Friend",
+          id: "hhfttfjj"
+        },
+        {
+          first_name: "Princess",
+          last_name: "Mensah",
+          middle_name: "Linda",
+          phone_number: '0244626767',
+          address: "Box Mc 1582, Takoradi",
+          email: "jfrimpong@yahoo.com",
+          type: "Friend",
+          id: "hhfjjjhuh"
+        },
+        {
+          first_name: "James",
+          last_name: "Frimpong",
+          middle_name: "Kwame",
+          phone_number: '0244626767',
+          address: "Box Mc 1582, Takoradi",
+          email: "jfrimpong@yahoo.com",
+          type: "Friend",
+          id: "uuui"
+        }
+      ],
+};
+
+const contactsReducer = (state = {initialState}, action) => {
+  switch (action.type) {
+    case "ADD_CONTACT":
+        return state;
+      break;
+  
+    default:
+      return state;
+  }
+}
+
+export default contactsReducer;
