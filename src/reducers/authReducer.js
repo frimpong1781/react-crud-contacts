8005c0ae03ccd7bb216df2f5ec9ed410a5edc266
frimpong1781 <frimpong1781@yahoo.com>
2020-11-28 14:13:40 +0000

    created authentication reducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {};
+
+const authReducer = (state = {initialState}, action) => {
+  switch (action.type) {
+    case "LOGIN":
+        return state;
+  
+    default:
+      return state;
+  }
+}
+
+export default authsReducer;
