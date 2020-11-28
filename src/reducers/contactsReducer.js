ade5f85144653c391fa2db897b34f6c252db82ab
frimpong1781 <frimpong1781@yahoo.com>
2020-11-28 14:48:25 +0000

    giving comments to codes

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -33,12 +33,17 @@ const initialState = {
       ],
 };
 
-const contactsReducer = (state = {initialState}, action) => {
+// Use the initialState as a default value
+const contactsReducer = (state = initialState, action) => {
+  // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
+    // Do something here based on the different types of actions
     case "ADD_CONTACT":
         return state;
   
     default:
+          // If this reducer doesn't recognize the action type, or doesn't
+          // care about this specific action, return the existing state unchanged
       return state;
   }
 }
