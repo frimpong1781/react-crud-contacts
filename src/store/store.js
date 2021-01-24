58ecfc6981f05b081faab875249924169d51355a
frimpong1781 <frimpong1781@yahoo.com>
2021-01-24 17:03:24 +0000

    Add firebase configuration to our store

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,27 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import contactsReducer from '../reducers/contactsReducer';
 import authReducer from '../reducers/authReducer';
+
+// New imports
+import firebaseObj from '../firebase/firebase';
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import thunk from 'redux-thunk';
 
 // for creating single reducer, use the below code
 // export const store = createStore(contactsReducer);
 
-// Use code below when creating multiples reducers
+// Allows as to combine multiple reducers and pass it to the store
+const reducers = combineReducers({
+    contactsState: contactsReducer,
+    auth: authReducer
+});
+
 export const store = createStore(
-    combineReducers({
-        contactsState: contactsReducer,
-        auth: authReducer
-    })
-)
+    reducers,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reactReduxFirebase(firebaseObj),
+        reduxFirestore(firebaseObj)
+    )
+);
