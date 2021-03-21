93df18a483f276b9f47999b6dc9dd564fe779846
frimpong1781 <frimpong1781@yahoo.com>
2021-03-21 00:05:49 +0000

    add auth Reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import contactsReducer from '../reducers/contactsReducer';
-import authReducer from '../reducers/authReducer';
 
 // New imports
 import firebaseObj from '../firebase/firebase';
-import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
-import { getFirestore, reduxFirestore } from 'redux-firestore';
+import { getFirebase, reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { getFirestore, reduxFirestore, firestoreReducer } from 'redux-firestore';
 import thunk from 'redux-thunk';
 
 // for creating single reducer, use the below code
@@ -14,7 +13,8 @@ import thunk from 'redux-thunk';
 // Allows as to combine multiple reducers and pass it to the store
 const reducers = combineReducers({
     contactsState: contactsReducer,
-    auth: authReducer
+    firestore: firestoreReducer,
+    firebase: firebaseReducer,
 });
 
 export const store = createStore(
