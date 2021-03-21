e19b5c4e0bbe7b9f5dcc342a1fd3f66fee812149
frimpong1781 <frimpong1781@yahoo.com>
2021-03-21 00:11:13 +0000

    connect to firebase

--- src/actions/contactsActions.js
+++ src/actions/contactsActions.js
@@ -3,25 +3,43 @@
 // Since this function would be used outside of this file, we need to export it as a named export
 
 export const addContact = (newContact) => {
-    newContact.id=Math.random().toString()
-    return {
-        type: "ADD_CONTACT",
-        payload: newContact
-    }
-}
+    return (dispatch, getState, { getFirestore }) => {
+        const firestore = getFirestore();
+        firestore
+      .collection("contacts")
+      .add(newContact)
+      .then((res) => {
+        console.log("Contact added successfully!", newContact);
+      })
+      .catch((err) => console.log("An error occured!", err));
+    };
+};
 
 // Action creator for deleting contact
 export const deleteContact = (contactId) => {
-    return {
-        type: "DELETE_CONTACT",
-        payload: contactId
-    }
-}
+    return (dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        firestore
+          .collection("contacts")
+          .doc(contactId)
+          .delete()
+          .then(() => {
+            console.log("Contact deleted successfully!");
+          })
+          .catch((err) => {
+            console.log("An error occured!", err);
+          })
+      };
+    };
 
 // Action creator for editing contact
 export const editContact = (updatedContact) => {
-    return {
-        type: "EDIT_CONTACT",
-        payload: updatedContact
-    }
-}
+    return (dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        firestore.collection("contacts").doc(updatedContact.id).update(updatedContact).then(() => {
+          console.log("Contact updated successfully!");
+        }).catch((err) => {
+          console.log("An error occured!", err);
+        })
+      }
+    };
