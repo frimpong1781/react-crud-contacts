b46594dd97b802516ae40c04b086ab978b814ef2
frimpong1781 <frimpong1781@yahoo.com>
2021-03-21 00:13:36 +0000

    displaying data

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import { compose } from 'redux';
 import {connect} from 'react-redux';
+import { firestoreConnect } from "react-redux-firebase";
 import {Container, Row} from 'react-bootstrap';
 import Contact from './Contact';
 
-const Contacts = (props) => {
+const Contacts = ({ firestoreData, requested, requesting, ...props }) => {
+    let whatToDisplay;
+    if (requesting) whatToDisplay = <p>Loading...</p>;
+    else if (requested && firestoreData.length === 0)
+      whatToDisplay = <p>No contacts added</p>;
+    else if (requested && firestoreData.length > 0)
+      whatToDisplay = firestoreData.map((contact) => {
+        return <Contact contactInfo={contact} key={contact.id} editContact={props.editContact} />
+      });
+
     return (
         <Container>
-            <Row>
-                {props.contactsData.map((contact) => {
-                    return (
-                        <Contact 
-                          contactInfo={contact} 
-                          key={contact.id} 
-                          editContact={props.editContact}
-                        />
-                    );
-                })}
-            </Row>
+            <Row>{whatToDisplay}</Row>
         </Container>
     );
-}
+};
 
 const mapStateToProps = (state) => ({
-    contactsData: state.contactsState.contacts
-})
-export default connect(mapStateToProps) (Contacts);
+    contactsData: state.contactsState.contacts,
+    firestoreData: state.firestore.ordered.contacts,
+    requesting: state.firestore.status.requesting.contacts,
+    requested: state.firestore.status.requested.contacts,
+  });
+
+  export default compose(
+    connect(mapStateToProps),
+    firestoreConnect(["contacts"])
+  )(Contacts);
+
